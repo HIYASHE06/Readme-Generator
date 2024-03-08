bf1b4a34a9e730de056ee2e03908444bf9c05f30
Nkosinathi Prince Hiyashe <nphiyashe@gmail.com>
2024-03-08 16:05:42 +0000

    Created a basic structure for the readme in generate markdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,45 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
-}
+  ## Description
+
+  ${data.description}
+  
+  ## Table of Contents 
+  
+  * Installation
+  * Usage
+  * License
+  * Contributing
+  * Tests
+  * Questions
+  
+  ## Installation
+  
+  ${data.installation}
+  
+  ## Usage 
+  
+  ${data.usage}
+  
+  ## License
+  
+  This project is license under the ${data.license} license.
+  
+  ## Contributing
+  
+  ${data.contributing}
+  
+  ## Tests
+  
+  ${data.tests}
+  
+  ## Questions
+  
+  If you have any questions about the repo, open an issue or contact ${data.username} directly at ${data.email}.
+  `;
+  }
+
+
 
 module.exports = generateMarkdown;
